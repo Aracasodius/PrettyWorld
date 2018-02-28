74ff9ce878d12ea0b0fc3f8f54a40d0fc27da98b
Maikel <maikelbuunen2000@outlook.com>
2018-02-28 18:17:58 +0100

    Maikel first Push

---
+++ [Server Host]/PrettyWorld Server Host V1/PrettyWorld Server Host V1/Server Host/ClientHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyWorld_Server_Host_V1.Server_Host
+{
+    class ClientHandler
+    {
+    }
+}
