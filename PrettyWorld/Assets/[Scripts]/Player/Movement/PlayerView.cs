8b2119533e126f59582b13148bd6b1a9574d3ada
Main_Desktop\Grootloops <wachtwoord44@gmail.com>
2018-03-01 14:27:42 +0100

    Project cleanup

---
+++ PrettyWorld/Assets/[Scripts]/Player/Movement/PlayerView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerView : MonoBehaviour
+{
+    [SerializeField] Transform playerBody;
+
+    [SerializeField] float mouseSensitivity;
+
+    float xAxisClamp = 0;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        RotateCamera();
+    }
+
+    private void FixedUpdate()
+    {
+        #region Interactable Region
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2 (Screen.width / 2, Screen.height / 2));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Interactable interactable = hit.collider.GetComponent<Interactable>();
+            if (interactable != null)
+            {
+                Debug.Log("I've hit an interactable object.");
+            }
+        }
+
+        #endregion
+    }
+
+    void RotateCamera()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        float rotAmountX = mouseX * mouseSensitivity;
+        float rotAmountY = mouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        Vector3 targetRotCam = transform.rotation.eulerAngles;
+        Vector3 targetRotBody = playerBody.rotation.eulerAngles;
+
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if (xAxisClamp > 90) { xAxisClamp = targetRotCam.x = 90; }
+        else if (xAxisClamp < -90) { xAxisClamp = -90; targetRotCam.x = 270; }
+
+        
+
+        transform.rotation = Quaternion.Euler(targetRotCam);
+        playerBody.rotation = Quaternion.Euler(targetRotBody);
+    }
+}
